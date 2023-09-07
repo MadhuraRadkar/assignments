9c0b8a605cf05c3b27103718d3a745d05e6c46bb
MadhuraRadkar <madhuraradkar@gmail.com>
2023-09-07 22:22:56 +0530

    update

--- assignments/Break1.cs
+++ assignments/Break1.cs
@@ -9,25 +9,32 @@ namespace assignments
 {
     internal class Break1
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
             // Display num between 1 to 20 avoid the num which are divisible by 3 or 5.
 
-            int i = 1;
-            while (i <= 20)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    i++;
-                    continue;
-                }
+            //int i = 1;
+            //while (i <= 20)
+            //{
+            //    if (i % 3 != 0 && i % 5 != 0)
+            //    {
 
-
-                    Console.WriteLine(i);
-                i++;
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
               
-            }
+            //}
           
+
+
+            // Using for loop
+            for(int i = 0; i <= 20; i++)
+            {
+                if(i % 3 !=0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
            
         }
     }
